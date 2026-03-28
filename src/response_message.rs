7378b6529f05d18344357ca4cdf16f0b6246a9b2
~kev1nxyzz <94573335+kev1n999@users.noreply.github.com>
2026-03-28 14:33:43 -0300

    fix indentation

--- src/response_message.rs
+++ src/response_message.rs
@@ -5,59 +5,59 @@ use std::collections::{HashMap};
 
 #[derive(Copy, Clone)]
 pub enum StatusCode {
-  Success,
-  Created,
-  BadRequest,
-  NotFound,
-  InternalServerError,
+    Success,
+    Created,
+    BadRequest,
+    NotFound,
+    InternalServerError,
 }
 
 pub struct ResponseMessage {
-  pub content: String,
+    pub content: String,
 }
 
 impl fmt::Display for StatusCode {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    match self {
-      StatusCode::Success => write!(f, "200 OK"),
-      StatusCode::Created => write!(f, "201 Created"),
-      StatusCode::BadRequest => write!(f, "400 Bad Request"),
-      StatusCode::NotFound => write!(f, "404 Not Found"),
-      StatusCode::InternalServerError => write!(f, "500 Internal Server Error"),
-    }
+      match self {
+        StatusCode::Success => write!(f, "200 OK"),
+        StatusCode::Created => write!(f, "201 Created"),
+        StatusCode::BadRequest => write!(f, "400 Bad Request"),
+        StatusCode::NotFound => write!(f, "404 Not Found"),
+        StatusCode::InternalServerError => write!(f, "500 Internal Server Error"),
+      }
   }
 }
 impl fmt::Display for ResponseMessage {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    write!(f, "Message: {}", self.content)
+      write!(f, "Message: {}", self.content)
   }
 }
 
 fn header_format(status_code: StatusCode, content_type: &str, content_error: &str) -> String {
-  format!(
-      "HTTP/1.1 {}\r\n\
-  Content-Type: {}\r\n\
-  Content-Length: {}\r\n\
-  Connection: close\r\n\
-  \r\n\
-  {}", status_code, content_type, content_error.len(), content_error)
+    format!(
+        "HTTP/1.1 {}\r\n\
+    Content-Type: {}\r\n\
+    Content-Length: {}\r\n\
+    Connection: close\r\n\
+    \r\n\
+    {}", status_code, content_type, content_error.len(), content_error)
 }
 pub fn create_response_header(content_type: &str, body_content: &str) -> HashMap<String, String> {
-  let mut headers_hash: HashMap<String, String> = HashMap::new();
-  headers_hash.insert("Content-Length".to_string(), body_content.len().to_string());
-  headers_hash.insert("Content-Type".to_string(), content_type.to_string());
-  headers_hash
+    let mut headers_hash: HashMap<String, String> = HashMap::new();
+    headers_hash.insert("Content-Length".to_string(), body_content.len().to_string());
+    headers_hash.insert("Content-Type".to_string(), content_type.to_string());
+    headers_hash
 }
 
 impl ResponseMessage {
   pub fn badrequest_error(content_error: &str, content_type: &str) -> Self {
-    ResponseMessage { content: header_format(StatusCode::BadRequest, content_type, content_error) }
+      ResponseMessage { content: header_format(StatusCode::BadRequest, content_type, content_error) }
   }
   pub fn not_found_error(content_error: &str, content_type: &str) -> Self {
-    ResponseMessage { content: header_format(StatusCode::NotFound, content_type, content_error) }
+      ResponseMessage { content: header_format(StatusCode::NotFound, content_type, content_error) }
   }
 }
 
 pub fn send_response_error(mut stream: &TcpStream, response_content: String) {
-  stream.write_all(&response_content.as_bytes()).unwrap();
+    stream.write_all(&response_content.as_bytes()).unwrap();
 }
